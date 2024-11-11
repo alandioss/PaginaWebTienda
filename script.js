88773d67d510885ac9a0a0ba2cd9276c2a69e06d
alan <alan_suarez76@hotmail.com>
2024-11-11 11:49:23 -0300

    toogle menu

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para alternar la visibilidad del menú
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show'); // Alterna la clase 'show' para mostrar/ocultar el menú
+}
